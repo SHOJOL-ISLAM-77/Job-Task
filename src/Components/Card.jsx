cb6cf1f48a7a23ad7dd4a0f2fe3d9d6920d0d7fe
SHOJOL-ISLAM-77 <sh0j0l3875891@gmail.com>
2024-01-12 18:07:26 +0600

    add landing page

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,13 +1,77 @@
+/* eslint-disable react/prop-types */
+import {
+  FaBuffer,
+  FaCalendarAlt,
+  FaClipboardList,
+  FaRegComments,
+} from "react-icons/fa";
+import { FaPaperclip } from "react-icons/fa6";
 
-
-const Card = () => {
-    return (
-        <div className="bg-white p-4 m-2 h-44 flex flex-col gap-4">
-            <div className="flex justify-between"> <img src="/man.png" className="h-3 w-3 rounded-full " alt="" /> <p>name</p></div>
-            <div></div>
-            <div></div>
+const Card = ({
+  name,
+  img,
+  viewer_name,
+  viewer_img,
+  viewer_Photos,
+  date,
+  description,
+  message,
+  attachment,
+}) => {
+  console.log(viewer_Photos);
+  return (
+    <div className="bg-slate-50 p-4 m-2 mb-5 h-44 flex flex-col gap-4 rounded-lg">
+      <div className="flex justify-between">
+        {" "}
+        <div className="flex items-center font-semibold gap-2">
+          <img src={img} className="h-10 w-10 rounded-full object-cover" />{" "}
+          <p>{name}</p>
         </div>
-    );
+        <div className="flex items-center font-semibold gap-2">
+          <img
+            src={viewer_img}
+            className="h-10 w-10 rounded-full object-cover"
+          />{" "}
+          <p>{viewer_name}</p>
+        </div>
+      </div>
+      <div className="flex justify-between items-center">
+        <div className="flex items-center gap-2">
+          {" "}
+          <FaBuffer className="text-xl" />
+          {description}
+        </div>
+        <div>
+          <div className="flex items-center bg-gray-200 rounded-lg p-[2px] gap-1">
+            <FaClipboardList /> 1/2
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center gap-5 justify-between">
+        <div className="flex items-center font-semibold gap-3">
+          {viewer_Photos?.map((photo) => (
+            <img
+              key={photo}
+              src={photo}
+              className="h-9 w-9 rounded-full object-cover"
+            />
+          ))}
+        </div>
+        <div>
+          <FaRegComments className="inline-block text-xl" />
+          {message}
+        </div>
+        <div className="cursor-pointer">
+          <FaPaperclip className="inline-block text-xl" />
+          {attachment}
+        </div>
+        <div className="flex items-center">
+          <FaCalendarAlt className="inline-block " />
+          {date}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,27 +1,165 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Card from "../Components/Card";
-import { data } from "autoprefixer";
+import "../Components/css.css";
 
 const Home = () => {
+  const [Cards, setCards] = useState([]);
+  const [incomplete, setIncomplete] = useState([]);
+  const [complete, setComplete] = useState([]);
+  const [doing, setDoing] = useState([]);
+  const [toDo, setToDo] = useState([]);
+  const [underReview, setUnderReview] = useState([]);
   useEffect(() => {
     fetch("/Json.json")
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        console.log(data);
+        setCards(data);
+      });
   }, []);
 
+  useEffect(() => {
+    const CompleteData = Cards.filter((card) => card.category == "Complete");
+    setComplete(CompleteData);
+
+    const TodoData = Cards.filter((card) => card.category == "To do");
+    setToDo(TodoData);
+
+    const IncompleteData = Cards.filter(
+      (card) => card.category == "Incomplete"
+    );
+    setIncomplete(IncompleteData);
+
+    const doingData = Cards.filter((card) => card.category == "Doing");
+    setDoing(doingData);
+
+    const UnderReview = Cards.filter((card) => card.category == "Under review");
+    setUnderReview(UnderReview);
+  }, [Cards]);
+
   return (
-    <div className=" overflow-x-scroll m-5 container mx-auto">
+    <div className=" overflow-x-scroll m-5 scroll-x">
       <div className="flex justify-between w-screen gap-12  border ">
         <div className="max-h-full bg-slate-200">
-          <div className="text-xl font-bold text-black px-10">Incomplete</div>
-          <div className="w-[400px] ">
-            <Card />
+          <div className="text-xl font-bold text-black px-10 py-3">Incomplete</div>
+          <div
+            style={{ height: "calc(100vh - 100px)" }}
+            className=" overflow-y-auto scroll-container overflow-x-hidden"
+          >
+            <div className="w-[400px]">
+              {incomplete?.map((card, i) => (
+                <Card
+                  key={i}
+                  name={card.name}
+                  img={card.img}
+                  viewer_name={card.viewer_name}
+                  viewer_img={card.viewer_img}
+                  viewer_Photos={card.viewer_photos}
+                  date={card.date}
+                  description={card.description}
+                  message={card.message_count}
+                  attachment={card.attachments_count}
+                ></Card>
+              ))}
+            </div>
           </div>
         </div>
-        <div className="w-[400px] ">a</div>
-        <div className="w-[400px] ">d</div>
-        <div className="w-[400px] ">f</div>
-        <div className="w-[400px] ">e</div>
+        <div className="max-h-full bg-slate-200">
+          <div className="text-xl font-bold text-black px-10 py-3">To Do</div>
+          <div
+            style={{ height: "calc(100vh - 100px)" }}
+            className=" overflow-y-auto scroll-container overflow-x-hidden"
+          >
+            <div className="w-[400px]">
+              {toDo?.map((card, i) => (
+                <Card
+                  key={i}
+                  name={card.name}
+                  img={card.img}
+                  viewer_name={card.viewer_name}
+                  viewer_img={card.viewer_img}
+                  viewer_Photos={card.viewer_photos}
+                  date={card.date}
+                  description={card.description}
+                  message={card.message_count}
+                  attachment={card.attachments_count}
+                ></Card>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="max-h-full bg-slate-200">
+          <div className="text-xl font-bold text-black px-10 py-3">Doing</div>
+          <div
+            style={{ height: "calc(100vh - 100px)" }}
+            className=" overflow-y-auto scroll-container overflow-x-hidden"
+          >
+            <div className="w-[400px]">
+              {doing?.map((card, i) => (
+                <Card
+                  key={i}
+                  name={card.name}
+                  img={card.img}
+                  viewer_name={card.viewer_name}
+                  viewer_img={card.viewer_img}
+                  viewer_Photos={card.viewer_photos}
+                  date={card.date}
+                  description={card.description}
+                  message={card.message_count}
+                  attachment={card.attachments_count}
+                ></Card>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="max-h-full bg-slate-200">
+          <div className="text-xl font-bold text-black px-10 py-3">Under Review</div>
+          <div
+            style={{ height: "calc(100vh - 100px)" }}
+            className=" overflow-y-auto scroll-container overflow-x-hidden"
+          >
+            <div className="w-[400px]">
+              {underReview?.map((card, i) => (
+                <Card
+                  key={i}
+                  name={card.name}
+                  img={card.img}
+                  viewer_name={card.viewer_name}
+                  viewer_img={card.viewer_img}
+                  viewer_Photos={card.viewer_photos}
+                  date={card.date}
+                  description={card.description}
+                  message={card.message_count}
+                  attachment={card.attachments_count}
+                ></Card>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="max-h-full bg-slate-200">
+          <div className="text-xl font-bold text-black px-10 py-3">Complete</div>
+          <div
+            style={{ height: "calc(100vh - 100px)" }}
+            className=" overflow-y-auto scroll-container overflow-x-hidden"
+          >
+            <div className="w-[400px]">
+              {complete?.map((card, i) => (
+                <Card
+                  key={i}
+                  name={card.name}
+                  img={card.img}
+                  viewer_name={card.viewer_name}
+                  viewer_img={card.viewer_img}
+                  viewer_Photos={card.viewer_photos}
+                  date={card.date}
+                  description={card.description}
+                  message={card.message_count}
+                  attachment={card.attachments_count}
+                ></Card>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
